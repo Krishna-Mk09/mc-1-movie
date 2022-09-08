fca5c4145c94b4aaaa094153cf5b990a73940a4c
M V K <krishna.mugala9@gmail.com>
2022-09-08 16:46:28 +0530

    added all getter and  setter for the fields in the movie class

--- src/main/java/com/jap/Movie.java
+++ src/main/java/com/jap/Movie.java
@@ -17,4 +17,36 @@ public class Movie {
         this.genre = genre;
         this.releaseDate = releaseDate;
     }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
